9f496e749d6da0f1e30958d496c8ae0facef4022
ElisabethCWY <110924227+ElisabethCWY@users.noreply.github.com>
2024-09-22 21:21:40 +0800

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,4 +1,4 @@
-var vg_1 = "malaysia_immigration.json";
+var vg_1 = "malaysian_immigration.json";
 vegaEmbed("#map", vg_1).then(function(result) {
 // Access the Vega view instance
 (https://vega.github.io/vega/docs/api/view/) as result.view
